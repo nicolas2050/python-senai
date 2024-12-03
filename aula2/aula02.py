8a957a08825f6a65a3d2a1a5d863e5d52ec8fbef
nicolas2050 <nicolasalvesdasillva20@gmail.com>
2024-12-03 09:57:56 -0300

    Add files via upload

---
+++ aula2/aula02.py
@@ -0,0 +1,11 @@
+print("bom dia rapaziada")
+print("muito obrigado ")
+nome_usuario  ="nicolas alves"   # tipo -> str = string
+idade_usuario = 20 #tipp -> int = integer 
+altura_igual  = 1.77 # tipo -> float =ponto flutuante
+maior_idade  = True # tipo = boolean 
+
+print("seja bem vindo nome_usuario",nome_usuario)
+print("sua idade é ", idade_usuario, "anos")
+print("sua altura é",altura_igual,"metros.")
+print("voce e amoir de idade",maior_idade)
